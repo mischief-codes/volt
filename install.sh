0e7d488b924bbbfea61541abf0df88e4c7cbcbac
Alex Segura <alex@lispm.dev>
2021-08-31 13:19:54 -0600

    Clean up install.sh, update ignore_files.txt

--- install.sh
+++ install.sh
@@ -1,31 +1,56 @@
 #!/bin/bash
-usage() { printf "Usage: $0 [-w] URBIT_PIER_DIRECTORY  \n(-w: flag to watch and live copy code)\n" 1>&2; exit 1; }
+set -e
+
+usage() {
+  printf "Usage: %s [-hwxv] URBIT_PIER_DIRECTORY
+(-h: show this help)
+(-w: flag to watch and live copy code)
+(-x: set excludes file, default=ignore_files.txt)
+(-v: verbose)
+" "${0}" 1>&2;
+  exit 1;
+}
 
 if [ $# -eq 0 ]; then
-    usage
-    exit 2
+  usage
+  exit 2
 fi
-PIER=$1
-EXCLUDE_FILE=ignore_files.txt
 
-while getopts "w" opt; do
-    case ${opt} in
-        w) WATCH_MODE="true"
-           PIER=$2
-           ;;
-        *) usage
-           ;;
-    esac
+pier=''
+verbose=false
+watch=false
+excludes=ignore_files.txt
+
+while getopts 'wxv:?h' opt;
+do
+  case "${opt}" in
+    w) watch=true ;;
+    x) excludes="${OPTARG}" ;;
+    v) verbose=true ;;
+    h|?) usage ;;
+  esac
 done
 
-if [ -z "$WATCH_MODE" ]; then
-    echo "Installed."
-    rsync -r --exclude-from=$EXCLUDE_FILE * $PIER/home/
-else
-   echo "Watching for changes to copy to ${PIER}..."
-   while [ 0 ]
-   do
+shift $((OPTIND-1))
+
+if $verbose ; then
+  set -x
+fi
+
+pier="$1"
+
+sync_files () {
+  rsync -r --exclude-from="${excludes}" ./* "${pier}"/home/
+}
+
+if $watch ; then
+  echo "Watching for changes to copy to ${pier}..."
+  while true
+  do
     sleep 0.8
-    rsync -r --exclude-from=$EXCLUDE_FILE * $PIER/home/
-   done
+    sync_files
+  done
+else
+  sync_files
+  echo "Installed."
 fi
