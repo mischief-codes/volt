e87f6122790d0b82edeb7ce713b4b503082ce8f2
mischief <y2k_mischief@proton.me>
2024-04-25 00:48:59 -0500

    working

--- ui/src/app.tsx
+++ ui/src/app.tsx
@@ -10,36 +10,40 @@ import ChannelDisplay from './components/status/ChannelDisplay';
 import { VoltProviderContextProvider } from './contexts/VoltProviderContext';
 import { HotWalletContextProvider } from './contexts/HotWalletContext';
 import { PaymentUpdateContextProvider } from './contexts/PaymentUpdateContext';
+import { ApiProvider } from './contexts/ApiContext';
 
 export function App() {
   return (
     <FeedbackContextProvider>
-      <HotWalletContextProvider>
-        <ChannelContextProvider>
-          <PaymentUpdateContextProvider>
-            <VoltProviderContextProvider>
-              <InvoiceContextProvider>
-                <main className="bg-slate-200 h-screen">
-                <div className="flex h-full overflow-y-hidden">
-                <div className="bg-zinc-100 w-1/2 pt-20">
-                    <Commands />
-                </div>
-                  <div className='flex flex-col h-full w-1/2'>
-                    <div className='h-1/3 bg-zinc-800 overflow-scroll rounded-b-sm'>
-                    <CommandFeedback />
-                    </div>
-                    <div className='h-2/3 bg-zinc-300 text-gray-500 rounded-sm'>
-                      <LiquidityDisplay/>
-                      <ChannelDisplay />
+      <ApiProvider>
+        <HotWalletContextProvider>
+          <ChannelContextProvider>
+            <PaymentUpdateContextProvider>
+              <VoltProviderContextProvider>
+                <InvoiceContextProvider>
+                  <main className="bg-slate-200 h-screen">
+                  <div className="flex h-full overflow-y-hidden">
+                  <div className="bg-zinc-100 w-1/2 pt-20">
+                      <Commands />
+                  </div>
+                    <div className='flex flex-col h-full w-1/2'>
+                      <div className='h-1/3 bg-zinc-800 overflow-scroll rounded-b-sm'>
+                      <CommandFeedback />
+                      </div>
+                      <div className='h-2/3 bg-zinc-300 text-gray-500 rounded-sm'>
+                        <LiquidityDisplay/>
+                        <ChannelDisplay />
+                      </div>
                     </div>
                   </div>
-                </div>
-                </main>
-              </InvoiceContextProvider>
-            </VoltProviderContextProvider>
-          </PaymentUpdateContextProvider>
-        </ChannelContextProvider>
-      </HotWalletContextProvider>
+                  </main>
+                </InvoiceContextProvider>
+              </VoltProviderContextProvider>
+            </PaymentUpdateContextProvider>
+          </ChannelContextProvider>
+        </HotWalletContextProvider>
+      </ApiProvider>
     </FeedbackContextProvider>
+
   );
 }
